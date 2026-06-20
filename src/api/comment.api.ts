3ae4e8a763e72291a89dc8ce92efd01c8b4acf91
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2026-06-20 12:37:31 +0530

    Comment Feature Added

---
+++ src/api/comment.api.ts
@@ -0,0 +1,23 @@
+import api from "../lib/axios";
+
+export const getCommentsByPostId = async (postId: string) => {
+  const response = await api.get(`comments/all/${postId}`);
+
+  return response.data.data;
+};
+
+export const createComment = async (postId: string, comment: string) => {
+  const response = await api.post(`/comments/create-comment/${postId}`, {
+    comment,
+  });
+
+  return response.data.data;
+};
+
+export const deleteComment = async (postId: string, commentId: string) => {
+  const response = await api.delete(
+    `/comments/delete-comment/post/${postId}/comment/${commentId}`,
+  );
+
+  return response.data;
+};
--- src/components/FeedPageComponent/FeedPost.tsx
+++ src/components/FeedPageComponent/FeedPost.tsx
@@ -5,9 +5,15 @@ import type { RootState } from "../../store/store";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { toggleLikePost } from "../../api/like.api";
-import { Heart, MessageCircle } from "lucide-react";
+import { Heart, MessageCircle, Reply, Trash2 } from "lucide-react";
 import { Link } from "react-router-dom";
 import Spinner from "../General/Spinner";
+import type { CommentType } from "../../types/comment";
+import {
+  createComment,
+  deleteComment,
+  getCommentsByPostId,
+} from "../../api/comment.api";
 
 interface FeedPostProps {
   post: FeedPostType;
@@ -20,6 +26,12 @@ function FeedPost({ post }: FeedPostProps) {
   const [likes, setLikes] = useState<string[]>(post.likes);
   const [likeCount, setLikeCount] = useState<number>(post.likeCount);
   const [loading, setLoading] = useState(false);
+
+  const [showComments, setShowComments] = useState<boolean>(false);
+  const [comments, setComments] = useState<CommentType[]>([]);
+  const [commentText, setCommentText] = useState("");
+  const [loadingComments, setLoadingComments] = useState<boolean>(false);
+  const [commentCount, setCommentCount] = useState(post.commentCount);
 
   const isLikedByMe = user ? likes.includes(user._id) : false;
 
@@ -53,6 +65,74 @@ function FeedPost({ post }: FeedPostProps) {
       setLoading(false);
     }
   };
+
+  const fetchComments = async () => {
+    try {
+      setLoadingComments(true);
+      const data = await getCommentsByPostId(post._id);
+      console.log(data);
+      setComments(data);
+    } catch (error: any) {
+      toast.error("failed to load comments");
+    } finally {
+      setLoadingComments(false);
+    }
+  };
+
+  const handleToggleComments = () => {
+    console.log("toggle comment clicked");
+
+    setShowComments((prev) => !prev);
+
+    if (!showComments) {
+      fetchComments();
+    }
+  };
+
+  const handleAddComments = async () => {
+    if (!user) {
+      toast.error("Login to comment");
+      return;
+    }
+
+    if (!commentText.trim()) {
+      toast.error("Comment cannot be empty");
+      return;
+    }
+
+    try {
+      setLoadingComments(true);
+      const newComment = await createComment(post._id, commentText);
+      setComments((prev) => [newComment, ...prev]);
+      setCommentText("");
+      setCommentCount((prev) => prev + 1);
+
+      toast.success("comment posted");
+      // setShowComments(false);
+    } catch (error: any) {
+      toast.error(error.message || "Failed to add comment..");
+    } finally {
+      setLoadingComments(false);
+    }
+  };
+
+  const handleDeleteComment = async (commentId: string) => {
+    try {
+      await deleteComment(post._id, commentId);
+
+      setComments((prev) =>
+        prev.filter((comment) => comment._id !== commentId),
+      );
+
+      setCommentCount((prev) => prev - 1);
+
+      toast.success("Comment deleted");
+    } catch (error: any) {
+      toast.error(error.message || "Failed to delete comment");
+    }
+  };
+
+  console.log(comments);
 
   return (
     <section className="px-8 py-6 border-b-2 mb-8 neo-container bg-secondary feed-section min-w-[60vw] border-r-2 border-violet-800 shadow-indigo-900 shadow-xs">
@@ -117,18 +197,110 @@ function FeedPost({ post }: FeedPostProps) {
                 </button>
               </div>
 
-              <div className="flex items-center gap-1 text-blue-500 hover:text-blue-700">
-                <button>
-                  <MessageCircle size={20} className="cursor-pointer" />
+              <div
+                className={`flex items-center gap-1 text-blue-500 hover:text-blue-700  `}
+              >
+                <button onClick={handleToggleComments}>
+                  <MessageCircle
+                    size={20}
+                    className={`cursor-pointer transition ${showComments ? "fill-blue-500 text-blue-500" : "text-blue-500"}`}
+                  />
                 </button>
                 <span className="font-medium text-gray-700">
-                  {post.commentCount}
+                  {commentCount}
                 </span>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {showComments && (
+        <div className="neo-card mt-4 bg-accent-1">
+          {/* Add Comment */}
+          <div className="neo-card bg-accent-2">
+            <textarea
+              value={commentText}
+              onChange={(e) => setCommentText(e.target.value)}
+              placeholder="Write a comment..."
+              className="w-full resize-none border-2 border-black bg-white p-2"
+              rows={1}
+            />
+            <button
+              onClick={handleAddComments}
+              className="mt-2 neo-button bg-button-2 px-2 py-1 hover-bg-button-1 ease-in-out font-bold"
+            >
+              Comment
+            </button>
+          </div>
+
+          {/* Comments List */}
+          <div className="mt-4 space-y-3">
+            {loadingComments ? (
+              <Spinner />
+            ) : comments.length === 0 ? (
+              <div className="neo-card bg-accent-2 text-center">
+                No comments yet
+              </div>
+            ) : (
+              comments.map((comment) => {
+                const canDelete =
+                  user?._id === comment.commentedBy._id ||
+                  user?._id === post.owner._id;
+
+                return (
+                  <div
+                    key={comment._id}
+                    className="neo-card bg-accent-2 p-2 flex justify-between items-center"
+                  >
+                    {/* Left continer */}
+                    <div className="flex items-center justify-center gap-1 shrink-0">
+                      <img
+                        src={comment.commentedBy.profileImage || defaultImage}
+                        alt={comment.commentedBy.username}
+                        className="w-10 h-10 rounded-full object-cover border-2 border-black"
+                      />
+                      <div>
+                        <p className="font-black text-sm uppercase tracking-normal">
+                          {comment.commentedBy.username || "Username"}
+                        </p>
+                        <p className="text-sm mt-1 break-words">
+                          {comment.comment}
+                        </p>
+                      </div>
+                    </div>
+                    {/* right container */}
+                    <div className="flex flex-col items-center gap-1 shrink-0">
+                      <span className="text-[10px] px-2 py-1 border-2 border-black bg-white font-bold">
+                        Posted On :{" "}
+                        {new Date(comment.createdAt).toLocaleDateString()}
+                      </span>
+                      <div className="flex items-center gap-3">
+                        <button
+                          className="neo-button px-2 py-1 flex items-center gap-1 text-xs font-bold hover:scale-105 transition"
+                          // onClick={() => handleReply(comment._id)}
+                        >
+                          <Reply size={14} />
+                          Reply
+                        </button>
+
+                        {canDelete && (
+                          <button
+                            onClick={() => handleDeleteComment(comment._id)}
+                            className="neo-button bg-red-300 px-2 py-1 flex items-center gap-1 text-xs font-bold text-red-800 hover:text-red-900 hover:scale-105 transition"
+                          >
+                            <Trash2 size={14} />
+                            Delete
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </div>
+      )}
     </section>
   );
 }

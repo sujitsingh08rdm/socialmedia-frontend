4812f39d33cc9766ca9c0de1af5d950d591c08b3
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2026-07-05 12:56:04 +0530

    search implementation done

--- src/api/feed.api.ts
+++ src/api/feed.api.ts
@@ -4,3 +4,11 @@ export const getFeedPost = async () => {
   const response = await api.get("/posts/all-posts");
   return response.data.data;
 };
+
+export const searchUser = async (query: string) => {
+  const response = await api.get(`/users/search?query=${query}`, {
+    withCredentials: true,
+  });
+
+  return response.data.data;
+};
